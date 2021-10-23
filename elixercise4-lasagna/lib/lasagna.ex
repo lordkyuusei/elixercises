993f9567535733b21bcada86827970329086a5a7
Kevin POIROT <kevin.poirot@epitech.eu>
2021-10-23 16:01:22 +0200

    feat(exercism): 4 and 5

--- elixercise4-lasagna/lib/lasagna.ex
+++ elixercise4-lasagna/lib/lasagna.ex
@@ -1,25 +1,23 @@
 defmodule Lasagna do
-  # Please define the 'expected_minutes_in_oven/0' function
+  @cooking_time 40
+  @time_per_layer 2
+
   def expected_minutes_in_oven() do
-    40
+    @cooking_time
   end
 
-  # Please define the 'remaining_minutes_in_oven/1' function
   def remaining_minutes_in_oven(minutes) do
     expected_minutes_in_oven() - minutes
   end
 
-  # Please define the 'preparation_time_in_minutes/1' function
   def preparation_time_in_minutes(layers_nbr) do
-    layers_nbr * 2
+    layers_nbr * @time_per_layer
   end
 
-  # Please define the 'total_time_in_minutes/2' function
   def total_time_in_minutes(layers_nbr, minutes) do
     preparation_time_in_minutes(layers_nbr) + minutes
   end
 
-  # Please define the 'alarm/0' function
   def alarm() do
     "Ding!"
   end
